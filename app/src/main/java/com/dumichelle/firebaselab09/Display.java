9d10051352963b731ce005b1a131e41ba801f95a
Michelle Claire Du <michelledu@MichellesAir726.fios-router.home>
2020-11-30 22:23:45 -0500

    Initial commit

---
+++ app/src/main/java/com/dumichelle/firebaselab09/Display.java
@@ -0,0 +1,103 @@
+package com.dumichelle.firebaselab09;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class Display extends AppCompatActivity   {
+
+    private FirebaseAuth auth;
+
+    private Button  add_room;
+    private EditText room_name;
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms = new ArrayList<>();
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        add_room = (Button) findViewById(R.id.btn_add_room);
+        room_name = (EditText) findViewById(R.id.room_name_edittext);
+        listView = (ListView) findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list_of_rooms);
+
+        listView.setAdapter(arrayAdapter);
+
+        add_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,Object> map = new HashMap<>();
+                map.put(room_name.getText().toString(),"");
+                root.updateChildren(map);
+            }
+        });
+
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while (i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+                list_of_rooms.clear();
+                list_of_rooms.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent1 = getIntent();
+                String toPut = intent1.getStringExtra("username");
+
+                Intent intent = new Intent(getApplicationContext(),Chat_Room.class);
+                intent.putExtra("room_name",((TextView)view).getText().toString() );
+                intent.putExtra("username", toPut);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
